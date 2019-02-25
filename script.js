5d651c864b1bcacc20b7393cc88e35009ad0e482
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2019-02-25 15:16:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// el.src.match("images/lemon_slot.PNG")
+// el.src = "images/orange_slot.PNG";
+// el.src = "images/cherry_slot.PNG";
+// document.getElementById('you-win').play();
+// el.src.match("images/cherry_slot.PNG")
+// el.src = "images/lemon_slot.PNG";
+// el.src.match("images/orange_slot.PNG")
+
+// cycle images in the following order: CHERRY --> LEMON --> ORANGE
+
+function nextImage(el){
+	if ( el.src.match("images/cherry_slot.PNG")){
+		// if the image is a cherry, change it to a lemon!
+		el.src = "images/lemon_slot.PNG";
+	} else if ( el.src.match("images/lemon_slot.PNG")){
+		// if the image is a lemon, change it to an orange!
+		el.src = "images/orange_slot.PNG";
+	} else if ( el.src.match("images/orange_slot.PNG")){
+		// if the image is an orange, change it to a cherry!
+		el.src = "images/cherry_slot.PNG";
+	} else {
+		// do nothing
+	}
+}
+// LEMON --> BAR --> CHERRY
+// function nextImage(el){}
+function nextImage2(el){
+	if (el.src.match("images/lemon_slot.PNG")){
+		// if the image is a lemon, change it to a bar!
+		el.src = "images/bar_slot.PNG";
+	} else if (el.src.match("images/bar_slot.PNG")){
+		// if the image is a bar, change it to an cherry!
+		el.src = "images/cherry_slot.PNG";
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		// if the image is an cherry, change it to a lemon!
+		el.src = "images/lemon_slot.PNG";
+	} else {
+		// do nothing
+	}
+}
+
+// ORANGE --> CHERRY --> SEVEN --> WATERMELON
+//function nextImage(el){}
+function nextImage3(el){
+	if (el.src.match("images/orange_slot.PNG")){
+		// if the image is a orange, change it to a cherry!
+		el.src = "images/cherry_slot.PNG";
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		// if the image is a cherry, change it to an seven!
+		el.src = "images/seven_slot.PNG";
+	} else if (el.src.match("images/seven_slot.PNG")){
+		// if the image is an seven, change it to a watermelon!
+		el.src = "images/watermelon_slot.PNG";
+	} else {
+		// do nothing
+	}
+}
+
+
+function checkSlot(el){
+	if (a.classList.contains("images/cherry_slot.PNG") && b.classList.contains("images/cherry_slot.PNG") && c.classList.contains("images/cherry_slot.PNG")){
+		mySound.play();
+		console.log("You win!");
+		a.classList.add("opaque");
+		b.classList.add("opaque");
+		c.classList.add("opaque");
+	}
+	
+			
+	else{
+		//i wish this function would work when all lights are yellow, or all are red...
+	}
+}
